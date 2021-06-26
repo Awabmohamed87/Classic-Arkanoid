49929a082f6427a744d56a4d18f9daae04d26736
Awabmohamed87 <62722055+Awabmohamed87@users.noreply.github.com>
2021-06-26 18:26:31 +0200

    final game

---
+++ src/com/company/Enemy.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Enemy {
+    private Random random = new Random();
+    protected int px =0 ,py;
+    protected int vx = 2,vy = 1;
+    private int health = 1;
+    private boolean isInverted =false;
+    private boolean isCircled =false;
+    private boolean isBossLaunched = false;
+
+    Image enemyShape;
+    Enemy(){
+        enemyShape = new ImageIcon("Data\\Power-ups\\Rock.png").getImage();
+
+    }
+
+    //set enemy position random in regular levels
+    public void setEnemyInitialPos() {
+        px = random.nextInt(700);
+        py = 0 - enemyShape.getHeight(null);
+    }
+    //set only boss position
+    public void setEnemyInitialPos(int py) {
+        px = random.nextInt(700);
+        this.py = 0 - py - enemyShape.getHeight(null);
+    }
+    //set position to enemies fallen from boss
+    public void setEnemyInitialPos(int px,int py) {
+        this.px = px;
+        this.py = py;
+    }
+    public void setHealth(int health){
+        this.health = health;
+    }
+    public int getHealth(){return health;}
+
+   //only boss move(doesn't go after paddle just stay on his range)
+    public void move(){
+        if(py>350) {
+            vy *= -1;
+        }
+        if(py<=3&&isBossLaunched){
+            vy *= -1;
+        }
+        if(px<=0 || px>=800-70) {
+            vx *= -1;
+        }
+        if(py>20){
+            isBossLaunched = true;
+        }
+
+        px += vx;
+        py += vy;
+    }
+
+    //move of the regular enemies to catch the paddle
+    public void move(Paddle paddle) {
+            if (paddle.paddle_xp + paddle.getImage().getWidth(null) / 2 < px && !isCircled) {
+                vx = -1;
+            } else if (paddle.paddle_xp + paddle.getImage().getWidth(null) / 2 > px && !isCircled) {
+                vx = 1;
+            } else {
+                vx = 0;
+            }
+            if (py > 400 && !isInverted) {
+                isCircled = true;
+                vy = -1;
+            } else if (py < 300 && vy < 0) {
+                isInverted = true;
+                vy = 1;
+            }
+            if (py > 400 && isInverted) {
+                isCircled = false;
+            }
+            if (isCircled) {
+                if (py >= 350) {
+                    vx = 1;
+                } else {
+                    vx = -1;
+                }
+            }
+            px += vx;
+            py += vy;
+        }
+}
